0d55b7c5bc07b5b16a03dfb8e855f97d65375093
wojciechzyla <62514065+wojciechzyla@users.noreply.github.com>
2021-11-21 14:14:43 +0100

    testy roznych wartosci parametrow

--- main.py
+++ main.py
@@ -23,19 +23,19 @@ def main():
         if k <= 100:
             return 1
 
-    gruszki = FruitTypeInfo(name="gruszki", quantity=742, planting_cost=0, base_price=[20]*30,
+    gruszki = FruitTypeInfo(name="gruszki", quantity=742, planting_cost=850, base_price=[3]*30,
                             wholesale_price=[3]*30, demand=[2]*10+[4]*20, min_market_sold=10,
                             multiplier=multiplier1)
 
-    jablka = FruitTypeInfo(name="jabłka", quantity=535, planting_cost=0, base_price=[20]*30,
+    jablka = FruitTypeInfo(name="jabłka", quantity=535, planting_cost=621, base_price=[3]*30,
                             wholesale_price=[3] * 30, demand=[5]*10+[8]*10+[3]*10, min_market_sold=12,
                             multiplier=multiplier1)
 
-    sliwki = FruitTypeInfo(name="sliwki", quantity=800, planting_cost=0, base_price=[20]*30,
+    sliwki = FruitTypeInfo(name="sliwki", quantity=800, planting_cost=916, base_price=[5]*30,
                            wholesale_price=[4]*30, demand=[10]*30, min_market_sold=12,
                            multiplier=multiplier2)
 
-    wisnie = FruitTypeInfo(name="wisnie", quantity=1000, planting_cost=0, base_price=[20]*30,
+    wisnie = FruitTypeInfo(name="wisnie", quantity=1000, planting_cost=1100, base_price=[7]*30,
                            wholesale_price=[5]*30, demand=[10]*30, min_market_sold=20,
                            multiplier=multiplier2)
 
@@ -80,9 +80,9 @@ def main():
     with open("rozwiazania.txt", "w") as f:
         f.write(txt_to_write)
 
-    sol, profit, (num_draws, num_ok_draws) = orchard.find_solution(T_start=1000, T_stop=20, iterations_in_temp=20,
+    sol, profit, (num_draws, num_ok_draws) = orchard.find_solution(T_start=5000, T_stop=1, iterations_in_temp=100,
                                                                    epsilon=2, iterations_epsilon=10, alpha=0.99,
-                                                                   neighbour_type=1, initial_sol=3)
+                                                                   neighbour_type=1, initial_sol=10)
     print(sol, profit)
     print("ile procent losowanych rozwiązań spełnia ograniczenia:")
     print(num_ok_draws/num_draws*100)
