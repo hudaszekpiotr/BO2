3311ba5072aa0ec43399a7d20db1dc94f80ca45d
Leonkiewicz <Leonkiewicz@student.agh.edu.pl>
2021-12-12 18:56:48 +0100

    wizualizacja i przeliczanie

--- main.py
+++ main.py
@@ -24,19 +24,19 @@ def main():
         if k <= 100:
             return 1
 
-    gruszki = FruitTypeInfo(name="gruszki", quantity=742, planting_cost=850, base_price=[3]*num_days,
+    gruszki = FruitTypeInfo(name="gruszki", quantity=742, planting_cost=85, base_price=[3]*num_days,
                             wholesale_price=[3]*num_days, demand=[2]*(num_days//3)+[4]*(num_days-num_days//3), min_market_sold=10,
                             multiplier=multiplier1)
 
-    jablka = FruitTypeInfo(name="jabłka", quantity=535, planting_cost=621, base_price=[3]*num_days,
+    jablka = FruitTypeInfo(name="jabłka", quantity=535, planting_cost=62, base_price=[3]*num_days,
                             wholesale_price=[3] * num_days, demand=[5]*(num_days//3)+[8]*(num_days//3)+[3]*(num_days-2*(num_days//3)), min_market_sold=12,
                             multiplier=multiplier1)
 
-    sliwki = FruitTypeInfo(name="sliwki", quantity=800, planting_cost=916, base_price=[5]*num_days,
+    sliwki = FruitTypeInfo(name="sliwki", quantity=800, planting_cost=91, base_price=[5]*num_days,
                            wholesale_price=[4]*num_days, demand=[10]*num_days, min_market_sold=12,
                            multiplier=multiplier2)
 
-    wisnie = FruitTypeInfo(name="wisnie", quantity=1000, planting_cost=1100, base_price=[7]*num_days,
+    wisnie = FruitTypeInfo(name="wisnie", quantity=1000, planting_cost=110, base_price=[7]*num_days,
                            wholesale_price=[5]*num_days, demand=[10]*num_days, min_market_sold=20,
                            multiplier=multiplier2)
 
@@ -69,7 +69,6 @@ def main():
     orchard = Orchard([wisnie, jablka, gruszki, sliwki], employee_cost, warehouse_cost, 100, 40, num_days)
 
     initial_population = orchard.create_initial_population()
-
     txt_to_write = ""
     for el in initial_population:
         txt_to_write += str(el[1])
@@ -81,14 +80,25 @@ def main():
     with open("rozwiazania.txt", "w") as f:
         f.write(txt_to_write)
 
-    """sol, profit, (num_draws, num_ok_draws) = orchard.simulated_annealing(T_start=1000, T_stop=100, iterations_in_temp=100,
-                                                                          epsilon=4, iterations_epsilon=10, alpha=0.99,
-                                                                          neighbour_type=1, initial_sol=2)
-    print(sol, profit)
-    print("ile procent losowanych rozwiązań spełnia ograniczenia:")
-    print(num_ok_draws/num_draws*100)"""
+    profit_lists = []
+    
+    # sol, profit, (num_draws, num_ok_draws), profit_list = orchard.simulated_annealing(T_start=1000, T_stop=1, iterations_in_temp=100,
+    #                                                                       epsilon=4, iterations_epsilon=10, alpha=0.99,
+    #                                                                       neighbour_type=1, initial_sol=2)
+    # print("Solution: {}".format(sol))
+    # print(profit)
+    # print("ile procent losowanych rozwiązań spełnia ograniczenia:")
+    # print(num_ok_draws/num_draws*100)
 
-    sol, profit = orchard.genetic_algorithm(max_iter_no_progress=100, max_iter=1000)
-    print(sol, profit)
+    for init_pop in range(11):
+        sol, profit, (num_draws, num_ok_draws), profit_list = orchard.simulated_annealing(T_start=500, T_stop=1, iterations_in_temp=10, epsilon=2, iterations_epsilon=10,
+                                                                alpha=0.99,
+                                                                neighbour_type=1, initial_sol=init_pop)
+        profit_lists.append(profit_list)
+
+
+    return profit_lists
+
+    # sol, profit = orchard.genetic_algorithm(max_iter_no_progress=1000, max_iter=10000)
 
 main()
